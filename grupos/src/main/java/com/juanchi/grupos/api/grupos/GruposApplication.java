005f46b71f91d3cd5f722c9722e76efc9d3979d3
Juan Del Angel <jmdelan2012@gmail.com>
2024-06-01 20:03:50 +0000

    proyecto inicial api

---
+++ grupos/src/main/java/com/juanchi/grupos/api/grupos/GruposApplication.java
@@ -0,0 +1,13 @@
+package com.juanchi.grupos.api.grupos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GruposApplication.class, args);
+	}
+
+}
---
+++ grupos/src/test/java/com/juanchi/grupos/api/grupos/GruposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanchi.grupos.api.grupos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
