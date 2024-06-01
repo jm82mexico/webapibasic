e342cc91b2d7974452798a7c521b83fba5953982
Juan Del Angel <jmdelan2012@gmail.com>
2024-06-01 23:15:50 +0000

    Especificar el request mappping

--- grupos/src/main/java/com/juanchi/grupos/api/grupos/controllers/ProductController.java
+++ grupos/src/main/java/com/juanchi/grupos/api/grupos/controllers/ProductController.java
@@ -11,12 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.juanchi.grupos.api.grupos.entities.Product;
 import com.juanchi.grupos.api.grupos.services.IProductService;
 
 @RestController
+@RequestMapping("/api/products")
 public class ProductController {
 
     @Autowired
