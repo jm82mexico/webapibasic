032c6f9428538a1e02507a1f0254be60f218f3ab
Juan Del Angel <jmdelan2012@gmail.com>
2024-06-03 02:00:54 +0000

    feat: Agregar validación de campos en el controlador de productos

--- grupos/src/main/java/com/juanchi/grupos/api/grupos/controllers/ProductController.java
+++ grupos/src/main/java/com/juanchi/grupos/api/grupos/controllers/ProductController.java
@@ -1,11 +1,14 @@
 package com.juanchi.grupos.api.grupos.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,12 +43,18 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> create(@Valid @RequestBody Product product){
+public ResponseEntity<?> create(@Valid @RequestBody Product product, BindingResult result){
+        if(result.hasErrors()){
+            return validation(result);
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> update(@Valid @RequestBody Product product, @PathVariable Long id){
+    public ResponseEntity<?> update(@Valid @RequestBody Product product, BindingResult result, @PathVariable Long id){
+        if(result.hasErrors()){
+            return validation(result);
+        }
         Optional<Product> productOptional = productService.update(id, product);
         if(productOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.CREATED).body(productOptional.orElseThrow());
@@ -53,12 +62,24 @@ public class ProductController {
         return ResponseEntity.notFound().build();
     }
     
+    
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id){
         return productService.findById(id)
-                .map(product -> {
-                    productService.delete(product);
-                    return ResponseEntity.ok().build();
-                }).orElse(ResponseEntity.notFound().build());
-    }    
+        .map(product -> {
+            productService.delete(product);
+            return ResponseEntity.ok().build();
+        }).orElse(ResponseEntity.notFound().build());
+    }   
+
+    private ResponseEntity<?> validation(BindingResult result) {
+        
+        Map<String, String> errors = new HashMap<>();
+
+        result.getFieldErrors().forEach(err -> {
+            errors.put(err.getField(), "El campo " + err.getField() + " " + err.getDefaultMessage());
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
 }
