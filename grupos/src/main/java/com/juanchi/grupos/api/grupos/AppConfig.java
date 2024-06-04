e070a62fbf9bc766b645817fd47bb4e230b0a95f
Juan Manuel Del Angel <jmdelan2012@gmail.com>
2024-06-04 19:06:52 +0000

    chore: Actualizar configuración de swagger

--- grupos/src/main/java/com/juanchi/grupos/api/grupos/AppConfig.java
+++ grupos/src/main/java/com/juanchi/grupos/api/grupos/AppConfig.java
@@ -1,11 +1,25 @@
 package com.juanchi.grupos.api.grupos;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 
 @Configuration
 @PropertySource("classpath:messages.properties")
 public class AppConfig {
 
+    @Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().info(new Info()
+				.title("Grupos API")
+				.version("1.0")
+				.description("API para la gestión de grupos de usuarios")				
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
+			
 }
--- grupos/src/main/java/com/juanchi/grupos/api/grupos/GruposApplication.java
+++ grupos/src/main/java/com/juanchi/grupos/api/grupos/GruposApplication.java
@@ -3,28 +3,13 @@ package com.juanchi.grupos.api.grupos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class GruposApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GruposApplication.class, args);
-	}
-
-	@Bean
-	public OpenAPI customOpenAPI() {
-		return new OpenAPI().info(new Info()
-				.title("Grupos API")
-				.version("1.0")
-				.description("API para la gestión de grupos de usuarios")				
-				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
-	}
-				
-			
+	}			
 
 }
